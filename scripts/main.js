1f6a38115edcee9f55dc8b36a8a14e5bd4539d78
Franco Cedillo <franco.cedillo@gmail.com>
2013-09-30 14:25:45 -0500

    hosted libraries, responsivity added, multiline messages in json

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,27 @@
+var tagsToReplace = {
+    '&': '&amp;',
+    '<': '&lt;',
+    '>': '&gt;'
+};
+
+function replaceTag(tag) {
+    return tagsToReplace[tag] || tag;
+}
+
+function safe_tags_replace(str) {
+    return str.replace(/[&<>]/g, replaceTag);
+}
+
 $(function() {
 	
+	var obj = eval ("(" + txt + ")");
+    
+  var instants = obj.instants;
+	
   var createRows = function() {
-		instants = obj.instants;
+		var instant = null;
 		for (var i = 0; i < instants.length; i++) {
-      var instant = obj.instants[i];
+      instant = instants[i];
 			$('table#instants > tbody').append('<tr data-row-key="'+(i+1)+'"><td>'+instant.startAt+'</td><td>'+instant.title+'</td></tr>');
     }
 	};
@@ -16,11 +34,30 @@ $(function() {
 			
 			var rowId = $(this).data("row-key");
 			var instantIndex = rowId - 1;
-			var command = instants[instantIndex][2]
-			$('#message').attr("value", command);
 			
-			var time = instants[instantIndex][0]
-			toInstant(time);
+			var instant = instants[instantIndex];
+			
+			$('div#message').html('');
+			
+			if (instant.hasOwnProperty('file')) {
+				var file = instant.file;
+			  $('div#message').html('<strong>file: ' + file + '</strong><br />');
+			}
+
+			if (instant.hasOwnProperty('messages')) {
+				var messages = instant.messages;
+				var message = '';
+				var i = 0;
+				$('div#message').append('<pre>');
+				for ( i; i < messages.length; i++ ) {
+      		message = instant.messages[i];
+					line = safe_tags_replace(message.line);
+					$('div#message pre').append(line + '<br />');
+    		}
+			}
+			
+			var startAt = instant.startAt;
+			toInstant(startAt);
 			
 		});
 	};
